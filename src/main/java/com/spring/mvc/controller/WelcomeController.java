56e09b00fd3ce35948ea6a761018dffc2d12ff05
Abhishek1652002 <abhishekparas77@gmail.com>
2026-03-21 22:02:06 +0530

    Initial commit

---
+++ src/main/java/com/spring/mvc/controller/WelcomeController.java
@@ -0,0 +1,23 @@
+package com.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping("/")
+	public String greeting() {
+		
+		System.out.println("welcome.controller.greeting()");
+		return "index";
+	}
+	
+	
+	
+
+}
+ 
---
+++ src/main/webapp/WEB-INF/JSP/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Spring MVC Project</title>
+</head>
+<body bgcolor="pink">
+
+<h1>Hello This is my first MVC Project</h1>
+
+</body>
+</html>
