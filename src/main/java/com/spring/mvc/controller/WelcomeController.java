0f69a6a8f914b4cab6679adb98e29211ae8bf670
Abhishek1652002 <abhishekparas77@gmail.com>
2026-03-24 20:35:21 +0530

    update

--- src/main/java/com/spring/mvc/controller/WelcomeController.java
+++ src/main/java/com/spring/mvc/controller/WelcomeController.java
@@ -2,11 +2,15 @@ package com.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @Controller
@@ -35,11 +39,47 @@ public class WelcomeController {
 	@GetMapping("/path-var/{id}")
 	public String pathVarExample(@PathVariable(name="id") int id, Model model) {
 
-		model.addAttribute("user", "Sorry this:"+id+" User is not found in our DB" );
+		model.addAttribute("firstCookie", "Sorry this:"+id+" User is not found in our DB" );
 
 		System.out.println("WelcomeController.Pathvar:" + id);
 
 		return "success";
+	}
+	
+//	
+//	@GetMapping("/getCookie")
+//	public String getCookieVal(@CookieValue(name = "JSESSIONID") String JSESSIONID, Model model) {
+//
+//		model.addAttribute("cookie", "Your JSESSIONID is : " + JSESSIONID);
+//
+//		System.out.println("JSESSIONID : " + JSESSIONID);
+//
+//		return "cookieVal";
+//	}
+	
+	@GetMapping("/getMyCookie")
+	public String getMyCookieVal(@CookieValue(name = "firstCookie",defaultValue="DefaultCookie") String firstCookie, Model model) {
+
+		model.addAttribute("firstcookie", firstCookie);
+
+		System.out.println("getMyCookieVal : " + firstCookie);
+
+		return "cookieVal";
+	}
+	
+	
+	
+	
+	@GetMapping("/setCookie")
+	public String setCookieVal(HttpServletResponse response, Model model) {
+
+		Cookie cookie1=new Cookie("firstCookie","SensitiveInformation");
+         cookie1.setMaxAge(10);//100 seconds
+         response.addCookie(cookie1);
+         
+         System.out.println("setcookie val");
+         
+		return "redirect:/getMyCookie";
 	}
 
 	
